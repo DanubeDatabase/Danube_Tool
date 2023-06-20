7d1986efa23e7ef408c0c75937c82f2f143c4a88
sfaraut <serge.faraut@toulouse.archi.fr>
2023-06-20 17:02:20 +0200

    DANUBE_tool - 2023-06-20-1702 - version 0.0.2d2   -  Additions and corrections in layer constants (DANUBE_config)

--- DANUBE_config/DANUBE_config.py
+++ DANUBE_config/DANUBE_config.py
@@ -35,12 +35,13 @@ GEOCLIMATE_INPUT_BUILDINGS_UTRF = 'GEOCLIMATE_INPUT_BUILDINGS_UTRF'
 FILOSOFI = 'FILOSOFI'
 
 DANUBE_LAYERS = {
-    "TOPO_BATI" : { "id": "TOPO_BATI" , "file_id": "BATIMENT", "type" : "INPUT", "layer" : None },
-    "TOPO_ACTIVITE" : { "id": "TOPO_ACTIVITE", "file_id": "ZONE_D_ACTIVITE_OU_D_INTERET", "type" : "INPUT", "layer" : None } ,
-    "GEO_BUILD_RSU" : { "id": "GEO_BUILD_RSU", "file_id": "rsu_indicators", "type" : "INPUT", "layer" : None } ,
-    "GEO_BUILD_URTF" : { "id": "GEO_BUILD_URTF", "file_id": "building_utrf", "type" : "INPUT", "layer" : None } ,
-    "GEO_BUILD_IND" : { "id": "GEO_BUILD_IND", "file_id": "building", "type" : "INPUT", "layer" : None } ,
-    "DANUBE_FILO" : { "id": "FILO", "file_id": "Filosofi2017_carreaux_nivNaturel_met", "type" : "INPUT", "layer" : None } ,
-    "DANUBE_BUILD_PREPROCESS" : { "id": "DANUBE_BUILD_PREPROCESS", "file_id": "DANUBE_building_preprocessed", "type" : "OUTPUT", "layer" : None } ,
-    "DANUBE_BUILD_DATA" : { "id": "DANUBE_BUILD_DATA", "file_id": "DANUBE_building_data", "type" : "OUTPUT", "layer" : None } ,
+    "TOPO_BATI" : { "id": "TOPO_BATI" , "file_id": "BATIMENT.shp", "type" : "INPUT", "layer" : None },
+    "TOPO_ACTIVITE" : { "id": "TOPO_ACTIVITE", "file_id": "ZONE_D_ACTIVITE_OU_D_INTERET.shp", "type" : "INPUT", "layer" : None } ,
+    "GEO_RSU_UTRF_FLOOR_AREA" : { "id": "GEO_RSU_UTRF_FLOOR_AREA", "file_id": "rsu_utrf_floor_area.geojson", "type" : "INPUT", "layer" : None } ,
+    "GEO_BUILD_URTF" : { "id": "GEO_BUILD_URTF", "file_id": "building_utrf.geojson", "type" : "INPUT", "layer" : None } ,
+    "GEO_ZONE" : { "id": "GEO_ZONE", "file_id": "zone.geoson", "type" : "INPUT", "layer" : None } ,
+    "GEO_BUILD_IND" : { "id": "GEO_BUILD_IND", "file_id": "building_indicators.geojson", "type" : "INPUT", "layer" : None } ,
+    "DANUBE_FILO" : { "id": "FILO", "file_id": "Filosofi2017_carreaux_nivNaturel_met.shp", "type" : "INPUT", "layer" : None } ,
+    "DANUBE_BUILD_PREPROCESS" : { "id": "DANUBE_BUILD_PREPROCESS", "file_id": "DANUBE_building_preprocessed.dpkg", "type" : "OUTPUT", "layer" : None } ,
+    "DANUBE_BUILD_DATA" : { "id": "DANUBE_BUILD_DATA", "file_id": "DANUBE_building_data.dpkg", "type" : "OUTPUT", "layer" : None } ,
 }
