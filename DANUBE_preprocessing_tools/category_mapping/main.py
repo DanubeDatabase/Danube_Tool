0e4345ad3d1f68f4214381eabdbaa5f772963e5c
Lorena Araujo <lorena.civil@gmail.com>
2023-07-05 17:29:55 +0200

    Define preprocess output layer as DANUBE_LAYERS['BUILD_PP_OUTPUT']['layer']. Remove archetype defintion from preprocessing. Create folder DANUBE_processing_tools to define archetypes.

--- DANUBE_preprocessing_tools/category_mapping/main.py
+++ DANUBE_preprocessing_tools/category_mapping/main.py
@@ -3,6 +3,7 @@ from category_mapping.map_year import main_cm_period
 from category_mapping.map_dept_code_insee import main_cm_territory
 from category_mapping.map_typology import main_cm_typology
 from category_mapping.map_usage import main_cm_usage
+from category_mapping.output_variable_layer import get_output_layer
 
 from category_mapping.map_reference.shared_ref import PATH_REF_FOLDER
 
@@ -21,11 +22,12 @@ def main_cm_category_mapping(df, DANUBE_LAYERS):
     main_cm_period(df)
     main_cm_territory(df, DANUBE_LAYERS)
 
+    output_layer = get_output_layer(df, DANUBE_LAYERS)
+
     print_log("+" * 100)
     print_log("--------------- END MAPPING DANUBE CATEGORIES -----------------")
     print_log("+" * 100)
 
-    return df
+    return output_layer
 
-if __name__ == '__console__':
-    main_cm_category_mapping(df)
+
--- DANUBE_preprocessing_tools/main_preprocess.py
+++ DANUBE_preprocessing_tools/main_preprocess.py
@@ -2,7 +2,7 @@ from config_show import timed_execution, print_log
 
 from data_consolidation.main import main_dc_data_consolidation
 from category_mapping.main import main_cm_category_mapping
-from archetype_definition.danube_archetype import main_arch
+from category_mapping.output_variable_layer import get_output_layer
 
 
 def main_preprocess_all(DANUBE_LAYERS):
@@ -12,20 +12,18 @@ def main_preprocess_all(DANUBE_LAYERS):
 
     DANUBE_LAYERS, df_output_dc = timed_execution(main_dc_data_consolidation, DANUBE_LAYERS)
 
-    df = timed_execution(main_cm_category_mapping, df_output_dc, DANUBE_LAYERS)
+    DANUBE_LAYERS['BUILD_PP_OUTPUT'] = {"id":None, "type":None,'layer':None }
+    DANUBE_LAYERS['BUILD_PP_OUTPUT']['layer']  = timed_execution(main_cm_category_mapping, df_output_dc, DANUBE_LAYERS)
+    print_log('\nDANUBE_LAYERS.keys(): ', DANUBE_LAYERS.keys())
 
-    layer_arch  = timed_execution(main_arch, df, DANUBE_LAYERS)
-    # DANUBE_LAYERS['DANUBE_BUILD_DATA']['layer'] = layer_arch
 
     print_log("#" * 100)
     print_log("--------------- END DANUBE PREPROCESSING -----------------")
     print_log("#" * 100)
 
+    return DANUBE_LAYERS
+
 
 def main_preprocess_all_plugin(self, parameters, context, feedback):
     """Run the main from the plugin giving self.DANUBE_tool_LAYERS as DANUBE_LAYERS"""
-    timed_execution(main_preprocess_all, self.DANUBE_tool_LAYERS)
-
-
-if __name__ == '__console__':
-    main_preprocess_all(DANUBE_LAYERS)
+    self.DANUBE_tool_LAYERS = timed_execution(main_preprocess_all, self.DANUBE_tool_LAYERS)
